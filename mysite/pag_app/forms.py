553af52be195f21591226b9ed517d4e6b0097a50
fszafran <filo1234@interia.pl>
2024-12-21 15:07:01 +0100

    zmniejszenie wyboru do msc/yr

--- mysite/pag_app/forms.py
+++ mysite/pag_app/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class DataForm(forms.Form):
     date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
-    data_type = forms.ChoiceField(choices=[('day', 'Day'), ('month', 'Month'), ('year', 'Year')], required=False)
+    data_type = forms.ChoiceField(choices=[('month', 'Month'), ('year', 'Year')], required=False)
